16fb70f0f7eb1e67732431733c8c85fe555ab9c5
Friddo34 <40137347+Friddo34@users.noreply.github.com>
2018-06-10 10:23:48 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+var prefix = ("*")
+
+bot.on('ready', function() {
+    bot.user.setActivity("BOT d'aide du MEC");
+    console.log("Connected");
+
+});
+
+bot.login(Token)
+
+bot.on('guildMemberAdd', member => {
+    var role = member.guild.roles.find('name', 'Invité')
+    member.addRole(role)
+
+})
+
+bot.on('guildMemberAdd', member => {
+    member.createDM().then(channel =>{
+        return channel.send("A Définir")
+    }).catch(console.error);
+});
+
+bot.on('messageReactionAdd', (reaction, user) => {
+    var role = reaction.message.guild.roles.find('name', 'Joueur');
+    var member = reaction.message.guild.member(user); 
+    if(reaction.emoji.name === '⚙'){
+    member.addRole(role);
+    console.log('Fait')}
+    })
