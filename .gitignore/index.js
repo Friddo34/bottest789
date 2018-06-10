770fce446dda905c88856a503f39d2891b1b97da
Friddo34 <40137347+Friddo34@users.noreply.github.com>
2018-06-10 20:59:11 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,6 +6,7 @@ var prefix = ("*")
 bot.on('ready', function() {
     bot.user.setActivity("BOT d'aide du MEC");
     console.log("Connected");
+    bot.channels.get("455445229584121856").send("Bot connecté au Discord")
 
 });
 
@@ -14,11 +15,13 @@ bot.login(process.env.TOKEN)
 bot.on('guildMemberAdd', member => {
     var role = member.guild.roles.find('name', 'Invités')
     member.addRole(role)
+     bot.channels.get("455445229584121856").send("Grade Invité donné à " +member.user.username)
 
 })
 
 bot.on('guildMemberAdd', member => {
     member.createDM().then(channel =>{
+        bot.channels.get("455445229584121856").send("MP envoyé à " +member.user.username)
         return channel.send("Bienvenue sur le Discord du Montpellier Esport Club ! \nVous faites maintenant partie des invités du Serveur. Vous pouvez profiter de cet accès pour commencer à échanger avec les membres dans les salons ouverts. Si vous souhaitez nous rejoindre, vous devez faire votre présentation et contacter un des membres du staff du MEC pour le signaler. Il faudra ensuite que vous remplissiez la fiche d'inscription (disponible sur le site : http://www.montpellier-esport-club.fr/rejoignez-nous/) et que vous vous acquittiez de la cotisation annuelle. \nSur ce, je vous laisse, et je vous souhaite de passer de bons moment en notre compagnie !")
     }).catch(console.error);
 });
